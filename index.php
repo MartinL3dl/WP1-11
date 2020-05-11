cd0ba4b8f1893a6fec04af5c2231f6acb8d888b9
MartinL3dl <martin.ledl@student.ossp.cz>
2020-05-11 17:57:39 +0200

    bolest nebyl samotný skript...ale vyhledávání Cringe obrázku mimoně
    za mé přežitý tohoto google pictures searche, jsem obdržel purporové srdce, a kříž
    
    ale za své přežití děkuji nebeskému Císaři, Ave Imperator

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <H1>NEVER AGAIN PLS CRINGE IS TOO STRONG</H1>
+ <br><br><br>   
+<?php
+define('max_number', 40);
+define('min_number', 5);
+define('numbers_count', rand(min_number, max_number));
+for ($i=0; $i < numbers_count ; $i++) { ?>
+  <img src="cringe.jpg" alt="testing">;
+<?php } ?>
+
+</body>
+</html>
